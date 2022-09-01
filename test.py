61f598f70ff6ad2bb7969b1a36a4f99a32d47af7
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-09-01 13:28:47 +0400

    fix

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from ffmpeg_input import FFMpegSource, VideoFrameEnvelope
 if __name__ == '__main__':
     s = FFMpegSource("/dev/video0", {"video_size": "320x240"})
     #     s = FFMpegSource("/home/ivan/video.mp4", {})
-    # while True:
-    #     try:
-    #         p = s.video_frame()
-    #         print(p.frame_width)
-    #         print(p.frame_height)
-    #         print(p.codec)
-    #     except BrokenPipeError:
-    #         break
+    while True:
+        try:
+            p = s.video_frame()
+            print(p.frame_width)
+            print(p.frame_height)
+            print(p.codec)
+        except BrokenPipeError:
+            break
