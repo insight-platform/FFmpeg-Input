0bfdd0b9fd8170c4ec780e2b106732accaacdaaf
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-09-10 18:08:49 +0400

    added utility method for fast vec<u8> > bytes conversion

--- test_decode.py
+++ test_decode.py
@@ -12,17 +12,16 @@ if __name__ == '__main__':
     s = FFMpegSource("dump.mp4", {"c:v": "v4l2m2m"}, len=100, decode=True)
     while True:
         try:
-            start = time.time()
             p = s.video_frame()
             res = p.payload_as_bytes()
             res = np.frombuffer(res, dtype=np.uint8)
             res = np.reshape(res, (p.frame_height, p.frame_width, 3))
             res = cv2.rotate(res[y - h:y + h, x:x + w], cv2.ROTATE_90_COUNTERCLOCKWISE)
             end = time.time()
-            cv2.imshow('Image', res)
-            print(p.queue_len, end - start)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            print(p.queue_len, end * 1000 - p.system_ts)
+            # cv2.imshow('Image', res)
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
         except BrokenPipeError:
             print("EOS")
             break
